e11000111927dfbf6cae7f9a7c274d91261a7ed6
Mustafa Vadnagarwala <vmustafa@outlook.com>
2014-08-22 12:42:41 +0530

    r programming assigment 2 - final version

--- cachematrix.R
+++ cachematrix.R
@@ -6,6 +6,9 @@
 ## USAGE:-
 ## step 1: cacheMatrix <- makeCacheMatrix(original matrix)
 ## step 2: inverseMatrix <- cacheSolve(cacheMatrix)
+## NOTE on USAGE: clubbing the 2 steps will prevent leveraging of cache feature
+## therefore avoid: inverseMatrix <- cacheSolve(makeCacheMatrix(matrix)) as it is 
+## not computationally efficient
 
 ## makeCacheMatrix function builds an object which acts as a repository of matrix
 ## and its inverse having the following capabilities:-
@@ -13,8 +16,13 @@
 ## inverse matrix data storage - xInv
 ## get / set functions for the matrix - getMatrix(), setMatrix(matrix)
 ## get / set functions for the inverse matrix - getInverse(), setInverse(matrix)
-
 makeCacheMatrix <- function(x = matrix()) {
+        ## if x not square matrix give caution message
+        if (nrow(x) != ncol(x)) {
+                message("Caution=>Not a square matrix. Inverse not defined.")
+        }
+        
+        ## get / set functions for matrix & inverse
         xInv <- NULL
         setMatrix <- function(y) {
                 x <<- y
@@ -23,6 +31,8 @@ makeCacheMatrix <- function(x = matrix()) {
         getMatrix <- function() x
         setInverse <- function(inverse) xInv <<- inverse
         getInverse <- function() xInv
+        
+        ## list of functions
         list(
                 getMatrix       = getMatrix,
                 getInverse      = getInverse,
@@ -35,7 +45,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve function is to be used by end user to obtain the inverse of a matrix
 ## input is the original matrix as a makeCacheMatrix object
 ## output is the inverse of the original matrix
-
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         xInv <- x$getInverse()
